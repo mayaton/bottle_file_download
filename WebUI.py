9f45fdd51719cd471510e38e508ff432e90b17f0
mayaton <alice.ne.jp@gmail.com>
2017-06-03 21:00:21 +0900

    Add ダウンロードボタン

--- WebUI.py
+++ WebUI.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import os
-from bottle import route,run
+from bottle import route,run,static_file
 from bottle import TEMPLATE_PATH, jinja2_template as template
 
 # HTML生成用Templateディレクトリの指定
 TEMPLATE_PATH.append("./views")
+file_dir="./"
 
 
 @route('/csv/')
 def hello():
-  files = os.listdir('./')
+  files = os.listdir(file_dir)
   return template('index', files=files)
 
+@route('/download/<file_path>')
+def static(file_path):
+    return static_file(file_path, root=file_dir, download=True)
+
 # WebServer起動及び設定
-run(host='localhost', port=80)
+run(host='localhost', port=80, debug=True, reloader=True)
 
